ef5858b9762d7a8528ae11f4e461b0f205bdd7b2
dyahayufebrianti <dyahayubrianti28@gmail.com>
2022-05-19 09:50:31 +0700

    coba

---
+++ app/Controllers/Dyahxirpl1.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Dyahxirpl1 extends Controller{
+    {
+        public function index()
+        {
+            return view('welcome_message');
+        }
+}
